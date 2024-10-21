650b9bcb980489eba17ccc5596633caa88e8491a
Stéphane DE LA TORRE <s.dlt@hotmail.fr>
2024-10-21 02:18:52 +0200

    feat(front): Add linting

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,13 +5,13 @@ import HelloWorld from "./components/HelloWorld.vue";
 <template>
   <div>
     <a href="https://gofiber.io" target="_blank">
-      <img src="./assets/fiber.svg" class="logo" alt="Fiber logo" />
+      <img src="./assets/fiber.svg" class="logo" alt="Fiber logo">
     </a>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo">
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo">
     </a>
   </div>
   <HelloWorld msg="Fiber + Vite + Vue" />
